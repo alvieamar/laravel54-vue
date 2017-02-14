eb0f9be544513a67a6fb339ba36a8535f73985a1
alvieamar <alvie.amar@bibo.com.ph>
2017-02-14 15:26:51 +0800

    setting up view and running app.js example.vue component \m/

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <div id="app">
+        <h1>Chat Room</h1>
+        <example></example>
+    </div>
+
+<script>
+    window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+    ]); ?>
+</script>
+<script src="/js/app.js" charset="utf-8"></script>
+</body>
+</html>
